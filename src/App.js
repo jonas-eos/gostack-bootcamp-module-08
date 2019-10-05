cbb9c3f66d8025b0e5a7d8b528cba8b703c51016
Jonas Evangelista <jonas.eos@outlook.com>
2019-10-05 19:18:11 -0300

    chore: useState hook

--- src/App.js
+++ src/App.js
@@ -1,7 +1,70 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-function App() {
-  return <h1>Hello world</h1>;
+/**
+ * Main function to load the app
+ * Sample function using useState hooks
+ * @function
+ * @default
+ */
+export default function App() {
+  /**
+   * State that containt techs
+   * @const
+   */
+  const [techs, setTech] = useState([
+    {
+      id: 1,
+      name: 'ReactJs',
+    },
+    {
+      id: 2,
+      name: 'React Native',
+    },
+    {
+      id: 3,
+      name: 'Node.js',
+    },
+  ]);
+
+  /**
+   * State that are used for insert new tech
+   * @const
+   */
+  const [newTech, setNewTech] = useState('');
+
+  /**
+   * Handler to Manipulates the process for saving new information to tech state
+   * the new tech id must be equal to the length + 1 of the tech state
+   * The new tech state must be reset to reset the input too.
+   * @const
+   * @function
+   */
+  const handleAddTech = () => {
+    setTech([
+      ...techs,
+      {
+        id: techs.length + 1,
+        name: newTech,
+      },
+    ]);
+    setNewTech('');
+  };
+
+  return (
+    <section>
+      <ul>
+        {techs.map(tech => (
+          <li key={tech.id}>{tech.name}</li>
+        ))}
+      </ul>
+      <input
+        type="text"
+        value={newTech}
+        onChange={e => setNewTech(e.target.value)}
+      />
+      <button type="button" onClick={handleAddTech}>
+        Add tech
+      </button>
+    </section>
+  );
 }
-
-export default App;
